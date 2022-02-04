05bf0b9e49fb043391cb01412134cc50c3c83bd9
Hatef <hatef.khodkar@hotmail.com>
2022-02-04 13:06:04 +0330

    make a simple call with shareReplay

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Observable, shareReplay } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private http: HttpClient) {}
+  posts$!: Observable<any[]>;
   title = 'cold-observers';
+
+  ngOnInit() {
+    this.posts$ = this.http
+      .get<any[]>(`https://jsonplaceholder.typicode.com/posts`)
+      .pipe(shareReplay());
+  }
+
+
+  // if you look at network tab in developer tools you see without share replays we call apis twice one for showing data and one for count
 }
